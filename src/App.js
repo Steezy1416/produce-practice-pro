50d4cac5c60123814fdf298692fa05de6f52681b
alexfortin <fortin.alex14@gmail.com>
2023-03-17 18:09:41 -0500

    created the starter code and style for the landing page as well added meta data for twitter and facebook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/landingPage";
+import landingPage from "./pages/LandingPage/landingPage";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/LandingPage/landingPage.js
@@ -0,0 +1,13 @@
+import "./landingStyle.css"
+import landingPageImage from "../../assets/landingPageImg.jpg"
+
+export default function LandingPage() {
+    return (
+        <section id="landing-page-container">
+            <div id="welcome-card">
+                <h1>Welcome To <br/>Produce Practice <span>Pro</span></h1>
+                <button id="get-started-btn">Get Started</button>
+            </div>
+        </section>
+    )
+}
