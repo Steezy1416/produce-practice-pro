4c2dd3b412e77d0e8580a2b050bc69d893e588a7
alexfortin <fortin.alex14@gmail.com>
2023-04-05 18:17:41 -0500

    styled home page for now but will probably come back to it later on, and made a puns array shuffle each time the home page re renders

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,17 +1,29 @@
 import { Link } from "react-router-dom"
 import puns from "./puns"
+import "./homePage.css"
 
 export default function HomePage() {
+
+    const shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    const shuffledPuns = shuffleArray(puns)
+
     return (
         <section id="home-page-container">
             <div id="pun-container">
-                <p>{puns[0]}</p>
+                <p>"{shuffledPuns[0]}"</p>
             </div>
             <div id="link-box-container">
-                <Link className="homePageLink" to={"/practice"}>Practice</Link>
-                <Link className="homePageLink" to={"/"}>Coming Soon</Link>
-                <Link className="homePageLink" to={"/"}>Coming Soon</Link>
-                <Link className="homePageLink" to={"/"}>Coming Soon</Link>
+                <Link className="homePageLink practiceLink" to={"/practice"}>Practice</Link>
+                <Link className="homePageLink flashcardLink" to={"/"}>Flashcards</Link>
+                <Link className="homePageLink searchLink" to={"/"}>Search</Link>
+                <Link className="homePageLink customPracticeLink" to={"/"}>Custom Practice</Link>
             </div>
         </section>
     )
