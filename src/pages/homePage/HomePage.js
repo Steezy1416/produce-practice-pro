350130114f3c6ff356c40f80297c99dc1fe0c9ed
alexfortin <fortin.alex14@gmail.com>
2023-04-07 03:17:30 -0500

    changed the layout of the home page by addid a randomized pun flashcard each time it loads, made an animation for the card

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,23 +1,43 @@
 import { Link } from "react-router-dom"
 import puns from "./puns"
 import "./homePage.css"
+import { useRef, useState } from "react"
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+const shuffledPuns = shuffleArray(puns)
+
+const [{ question, answer }] = shuffledPuns
 
 export default function HomePage() {
 
-    const shuffleArray = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
+    const [onQuestion, setOnQuestion] = useState(true)
+    const punContainer = useRef(null)
 
-    const shuffledPuns = shuffleArray(puns)
+    const handleCardClick = () => {
+        setOnQuestion(!onQuestion)
+        punContainer.current.classList.toggle("clicked-pun-container")
+        punContainer.current.addEventListener("animationend", () => {
+            punContainer.current.classList.remove("clicked-pun-container");
+        });
+
+    }
 
     return (
         <section id="home-page-container">
-            <div id="pun-container">
-                <p>"{shuffledPuns[0]}"</p>
+            <div ref={punContainer} onClick={handleCardClick} id="pun-container">
+                <p id="pun-text">
+                    {onQuestion
+                        ? question
+                        : answer
+                    }
+                </p>
             </div>
             <div id="link-box-container">
                 <Link className="homePageLink practiceLink" to={"/practice"}>Practice</Link>
--- src/pages/homePage/puns.js
+++ src/pages/homePage/puns.js
@@ -1,11 +1,32 @@
 const puns = [
-    "What did the grape say when it got stepped on all the time? Nothing, it just let out a little wine!",
-    "Why did the orange stop in the middle of the road? Because it ran out of juice!",
-    "Why did the watermelon break up with the honeydew? It just didn't find it a-peeling anymore.",
-    "Why did the lemon disown its son? Because he was a little tart!",
-    "Why don't melons ever get married? Because they cantaloupe!",
-    "What do you call a sad strawberry? A blueberry!",
-    "Why did the peach break up with the banana? It just couldn't find a-peel in the relationship!"
+    {
+        question: "What did the grape say when it got stepped on all the time?",
+        answer: "Nothing, it just let out a little wine!"
+    },
+    {
+        question: "Why did the orange stop in the middle of the road?",
+        answer: "Because it ran out of juice!"
+    },
+    {
+        question: "Why did the watermelon break up with the honeydew?",
+        answer: "It just didn't find it a-peeling anymore."
+    },
+    {
+        question: "Why did the lemon disown its son?",
+        answer: "Because he was a little tart!"
+    },
+    {
+        question: "Why don't melons ever get married?",
+        answer: "Because they cantaloupe!"
+    },
+    {
+        question: "What do you call a sad strawberry?",
+        answer: "A blueberry!"
+    },
+    {
+        question: "Why did the peach break up with the banana?",
+        answer: "It just couldn't find a-peel in the relationship!"
+    },
 ]
 
 export default puns
